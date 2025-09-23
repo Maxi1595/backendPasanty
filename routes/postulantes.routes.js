6c860a15463e576decfe18cf81ab30c23fef5211
Maxi <mbrizue@gmail.com>
2025-09-23 20:47:43 -0300

    se me olvido, agregar esto en el commit anterior

--- routes/postulantes.routes.js
+++ routes/postulantes.routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { verPorstulantes, buscarPorVacante, buscarPorPasante, crearPostulacion, eliminarPostulacion } = require ('../controllers/postulante.controller');
+const { verPorstulantes, buscarPorVacante, buscarPorPasante, crearPostulacion, eliminarPostulacion, actualizarEstado, buscarEstado } = require ('../controllers/postulante.controller');
 const { verificarToken, verificarRol } = require ('../middlewares/auth.middleware');
 
 router.get('/verTodos', verificarToken, verificarRol(5), verPorstulantes);
-router.get('/verPorVacante', verificarToken, verificarRol(5), buscarPorVacante);
-router.get('/verPorPasante', verificarToken, verificarRol(5), buscarPorPasante);
-router.post('/postularse', verificarToken, verificarRol(3), crearPostulacion);
-router.delete('/eliminarPostulacion', verificarToken, verificarRol(5), eliminarPostulacion);
+router.get('/verPorVacante/:id', verificarToken, verificarRol(5), buscarPorVacante);
+router.get('/verPorPasante/:id', verificarToken, verificarRol(5), buscarPorPasante);
+router.get('/verEstado', verificarToken, verificarRol(3), buscarEstado)
+router.post('/postularse/:id', verificarToken, verificarRol(3), crearPostulacion);
+router.delete('/eliminarPostulacion/:id', verificarToken, verificarRol(5), eliminarPostulacion);
+router.patch('/cambiarEstado/:id', verificarToken, verificarRol(5), actualizarEstado);
 
 module.exports = router;
