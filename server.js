2130d0f167ca6bf383f1e978fe5ee9a94cee1bf5
Maxi <mbrizue@gmail.com>
2026-07-24 19:04:46 -0300

    ajuste server

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(helmet());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://frontend-pasanty-mjfh.vercel.app'
+    'https://frontend-pasanty-mjfh-pasanty.vercel.app'
   ]
 }));
 
